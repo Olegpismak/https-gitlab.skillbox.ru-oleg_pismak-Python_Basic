a150b57c928486a214be0d9b50cc70e1184a75ea
Oleg Pismak <treverosgeorgi@gmail.com>
2023-08-09 09:07:27 +0300

    !

--- Module14/04_coin2/main.py
+++ Module14/04_coin2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def money():
+
+    money = (coordinat_X ** 2 + coordinat_Y ** 2) ** 0.5
+    if money <= radius:
+        print("\nМонетка где-то рядом")
+    else:
+        print("\nМонетки в области нет")
+
+
+coordinat_X = float(input('Введите координату Х: '))
+coordinat_Y = float(input('Введите координату Y: '))
+radius = float(input('Введите радиус окружности: '))
+
+money()
--- Module14/05_years/main.py
+++ Module14/05_years/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+years = int(input("Введите первый год: "))
+years2 = int(input("Введите второй год: "))
+print("Годы от,", years, "до", years2, " с тремя одинаковыми цифрами:")
+for i in range(years, years2+1):
+    first_num, second_num, third_num, four_num = i // 1000, i // 100 % 10, i // 10 % 10, i % 10
+    if ((first_num == second_num == third_num)
+            or (second_num == third_num == four_num)
+            or (third_num == four_num == first_num)
+            or (first_num == second_num == four_num)):
+        print(first_num * 1000 + second_num * 100 + third_num * 10 + four_num)
