0920bd74305c49165f19cfaafd87a9cfb10c8f31
Oleg Pismak <treverosgeorgi@gmail.com>
2023-08-09 08:35:39 +0300

    3

--- Module14/02_sum_and_dif/main.py
+++ Module14/02_sum_and_dif/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def suma_number(a):
+
+   return sum(map(int, list(a)))
+
+def count_number(a):
+
+   return len(a)
+
+def differ(a, b):
+
+   return a - b
+
+n = input('Введите число: ')
+
+print('Сумма чисел:', suma_number(n))
+
+print('Количество цифр в числе:', count_number(n))
+
+print('Разность суммы и количества цифр:', differ(suma_number(n), count_number(n)))
