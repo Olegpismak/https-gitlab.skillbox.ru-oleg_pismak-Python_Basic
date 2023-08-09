49f0c9e04957058eae414432a7b14f54345dea7f
Oleg Pismak <treverosgeorgi@gmail.com>
2023-08-09 08:38:39 +0300

    !

--- Module14/03_least_divisor/main.py
+++ Module14/03_least_divisor/main.py
@@ -1,5 +1,8 @@
-number = int(input('Введите число: '))
-i = 2
-while number % i != 0:
-    i += 1
-print('Наименьший делитель, отличный от единицы:', i)
+def divider():
+    number = int(input('Введите число: '))
+    i = 2
+    while number % i != 0:
+        i += 1
+    print('Наименьший делитель, отличный от единицы:', i)
+
+divider()
